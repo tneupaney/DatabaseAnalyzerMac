85b4e48e703545e41b60137645fe0db90c0501ce
Tushar Neupaney <tushar.neupaney@gmail.com>
2025-07-15 13:30:00 +1000

    Initial Commit

---
+++ DatabaseAnalyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DatabaseAnalyzer
+//
+//  Created by Tushar Neupaney on 15/7/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DatabaseAnalyzer/DatabaseAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatabaseAnalyzerApp.swift
+//  DatabaseAnalyzer
+//
+//  Created by Tushar Neupaney on 15/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct DatabaseAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
